049c90255c5905c5b42b5d9f5fd7a01ae97edb54
Bhargav Kodali <kodalibhargav019@gmail.com>
2024-10-16 13:50:01 -0700

    redo

--- main.tf
+++ main.tf
@@ -30,17 +30,17 @@ resource "aws_security_group"  "jenkins_security"{
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port   = -1
+  #   to_port     = -1
+  #   protocol    = "icmp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
    ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress{
