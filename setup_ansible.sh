69701c78153e87c7707bd064b7d098718a34e05d
Bhargav Kodali <kodalibhargav019@gmail.com>
2024-10-18 10:55:41 -0700

    install node

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -1,3 +1,14 @@
-
+#!/bin/bash
 cat .ssh/id_rsa.pub
 cat private-key.pem
+
+
+curl -fsSL https://fnm.vercel.app/install | bash
+# activate fnm
+source ~/.bashrc
+# download and install Node.js
+fnm use --install-if-missing 20
+# verifies the right Node.js version is in the environment
+node -v # should print `v20.18.0`
+# verifies the right npm version is in the environment
+npm -v #
